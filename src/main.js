36b4a396913300c156040507a908b8dd0d695573
autumnFish <517729329@qq.com>
2018-08-25 11:50:51 +0800

    2018-8-25 上午记录

--- src/main.js
+++ src/main.js
@@ -5,10 +5,42 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 // 导入首页的组件
 import Index from './components/01.index.vue';
+// 导入详情页的组件
+import Detail from './components/02.productDetail.vue';
+
+// 导入 elementui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 记得要use一下 才会把Elementui中的内容注册到Vue上面
+Vue.use(ElementUI);
 
 // 注册VueRouter(类似于Express的中间件语法)
 // 传送门:https://router.vuejs.org/zh/guide/#html JavaScript分类的第0行
 Vue.use(VueRouter);
+
+// 导入 懒加载 vue插件
+import VueLazyload from 'vue-lazyload'
+// 注册到Vue上
+// or with options
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  // 错误
+  // error: 'dist/error.png',
+  // 加载中
+  // 图片也要当做模块导入
+  loading: require('./assets/img/loadingDog.gif'),
+  attempt: 1
+})
+
+
+// 注册全局过滤器
+// 依赖于 moment.js
+import moment from 'moment';
+// 注册
+Vue.filter('filterDate', function(val){
+  return moment(val).format("YYYY年MM月DD日");
+})
+
 
 // 定义路由规则
 let routes = [
@@ -18,12 +50,18 @@ let routes = [
     path: '/',
     // component: Index,
     // 解析到/ 直接修改路由地址为/index
-    redirect:'/index'
+    redirect: '/index'
   },
   // 首页规则
   {
     path: '/index',
     component: Index,
+  },
+  // 详情页则
+  // 动态路由匹配:https://router.vuejs.org/zh/guide/essentials/dynamic-matching.html
+  {
+    path: '/detail/:id',
+    component: Detail,
   },
 ]
 
@@ -31,7 +69,7 @@ let routes = [
 // routes key 是固定的 
 // 所以我们才可以用这种快速赋值
 let router = new VueRouter({
-  routes:routes
+  routes: routes
 })
 
 // 挂在到 Vue示例上面
